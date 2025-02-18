6b743b1207d6f41df9b09ef2b54fae73ddc170c4
Manuel Ramallo Díaz <32164753+ManuelRamallo@users.noreply.github.com>
2025-02-18 21:07:37 +0100

    delete site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://manuelramallo.github.io',
   integrations: [tailwind()],
 });
